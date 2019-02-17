9652de40962e9885135ad23bfcf7b5d8e7aa53f0
kali <alik9807274@gmail.com>
2019-02-17 13:35:52 -0800

    LP OCR

--- licencePlateOCR/main.py
+++ licencePlateOCR/main.py
@@ -9,41 +9,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
+'''
+temp
+'''
 
-img = cv2.imread('1.png',0)
-edges = cv2.Canny(img,100,200)
+import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
 
-# plt.subplot(121),plt.imshow(img,cmap = 'gray')
-# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-# plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-# plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-# plt.show()
-# print(edges.shape)
-# plt.rcParams['figure.figsize'] = [16, 8]
-# plt.imshow(edges,cmap='gray')
-# plt.show()
+im = Image.open('temp.jpg')  # the second one
 
+# im = im.filter(ImageFilter.MedianFilter())
+# enhancer = ImageEnhance.Contrast(im)
+# im = enhancer.enhance(2)
+# im = im.convert('1')
+# im.save('temp2.jpg')
 
+text = pytesseract.image_to_string(im)
+print(text)
 
-img = cv2.imread('1.png')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-
-edgesi = cv2.Canny(img,600,600)
-edgesg = cv2.Canny(gray,600,600)
-
-
-#img = img[:, :, 0]
-
-sideBySide = np.hstack((edgesi,edgesg))
-
-#cv2.imshow('image',img)
-#cv2.imshow('image',gray)
-cv2.imshow('image',sideBySide)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-
-
-
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
